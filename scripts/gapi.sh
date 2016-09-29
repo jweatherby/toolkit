8973899c6d1782f2e44e3e7535aaf3f8239676a4
Jamie Weatherby <jamie.weatherby@gmail.com>
2016-09-29 11:11:44 -0400

    Add short rm_whitespace script

--- scripts/gapi.sh
+++ scripts/gapi.sh
@@ -23,10 +23,10 @@ fi
 
 if [ "$env" = "live" ]; then
     echo "WARNING: Using gapi LIVE (production)" 1>&2
-    CONFIG_KEY="live_532c2dccb163cdeb0e11103a5bdb26263fa98446"
+    CONFIG_KEY=""
 else
     echo "Using gapi TEST (sandbox)" 1>&2
-    CONFIG_KEY="test_b97026e3d6d9f6de89aa2ac3b77e3813d61f9e5e"
+    CONFIG_KEY=""
 fi
 
 if [ "$env" = "local" ]; then
